d172f15b0f46adebda15072500e95f2c2ca05661
Martins <chimamartins55@gmail.com>
2026-09-16 16:16:03 +0100

    preparing for render deployment

--- my-backend/index.js
+++ my-backend/index.js
@@ -11,7 +11,6 @@ const authRoute = require("./routes/authRoutes");
 const meRoute = require("./routes/meRoutes");
 const emailRoutes = require("./routes/emailverifyRoutes");
 
-const PORT = 3000;
 // middleware (lets us read JSON)
 app.use(express.json());
 
@@ -21,6 +20,8 @@ app.use("/me", meRoute);
 app.use("/verify-email", emailRoutes);
 
 // start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
